addd8c3c8ca71b2cf8912c76573f06d392d332dd
version-1 <j.onodera26@gmail.com>
2020-11-30 00:12:13 -0800

    adjust sidebar

--- front/src/components/tempaltes/Sidebar.tsx
+++ front/src/components/tempaltes/Sidebar.tsx
@@ -22,17 +22,18 @@ const Sidebar = () => {
       <Menu
         mode="inline"
         defaultSelectedKeys={[`1`]}
-        defaultOpenKeys={[`sub1`]}
+        defaultOpenKeys={['tweet']}
         style={{ height: `100%`, borderRight: 0 }}
       >
-        <SubMenu key="sub2" icon={<LaptopOutlined />} title="ツイート">
-          <Menu.Item key="5">一覧</Menu.Item>
-          <Menu.Item key="6">グループ</Menu.Item>
+        <Menu.Item key="1">ダッシュボード</Menu.Item>
+        <SubMenu key="tweet" icon={<LaptopOutlined />} title="ツイート">
+          <Menu.Item key="2">一覧</Menu.Item>
+          <Menu.Item key="3">グループ</Menu.Item>
         </SubMenu>
-        <Menu.Item key="6">スケジュール</Menu.Item>
-        <SubMenu key="sub1" icon={<UserOutlined />} title="ユーザ">
-          <Menu.Item key="1">プロフィール</Menu.Item>
-          <Menu.Item key="2">設定</Menu.Item>
+        <Menu.Item key="4">スケジュール</Menu.Item>
+        <SubMenu key="user" icon={<UserOutlined />} title="ユーザ">
+          <Menu.Item key="5">プロフィール</Menu.Item>
+          <Menu.Item key="6">設定</Menu.Item>
         </SubMenu>
         <Menu.Item key="9" onClick={onLogout}>
           ログアウト
